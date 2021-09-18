682e7060557b119535e185fbcb65e60ad1193a32
Sam Bean <sam.h.bean@gmail.com>
2021-09-18 00:34:33 -0400

    terraform the container registry

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "aks_goof" {
   name     = "aks-goof-k8s-resources"
   location = "East US"
 }
 
-resource "azurerm_kubernetes_cluster" "example" {
+resource "azurerm_kubernetes_cluster" "aks_goof" {
   name                = "aks-goof-k8s"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.aks_goof.location
+  resource_group_name = azurerm_resource_group.aks_goof.name
   dns_prefix          = "akf-goof-k8s"
 
   default_node_pool {
