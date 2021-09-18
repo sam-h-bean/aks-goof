309a96347bb22a2280474c9838772835c432a7cc
Sam Bean <sam.h.bean@gmail.com>
2021-09-18 01:14:22 -0400

    love me some race conditions

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -3,4 +3,5 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.aks_goof.name
   location            = azurerm_resource_group.aks_goof.location
   sku = "Premium"
+  admin_enabled = true
 }
