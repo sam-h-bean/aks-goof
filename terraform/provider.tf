72bd096346551139b39b8b92301c7242eb672e23
Sam Bean <sam.h.bean@gmail.com>
2021-09-18 00:20:59 -0400

    back to no required_providers

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.0.0"
-    }
-  }
-}
-
 provider "azurerm" {
+  version = "=2.0.0"
   features {}
 }
