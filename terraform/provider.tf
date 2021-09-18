9d7edb89d3611384f61fdd730668f04c62a2916d
Sam Bean <sam.h.bean@gmail.com>
2021-09-18 00:09:12 -0400

    only use provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
+  version = "=2.0.0"
   features {}
 }
