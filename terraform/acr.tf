412e5ae22acc0779cc2e11bfab33fbca731b9a40
Sam Bean <sam.h.bean@gmail.com>
2021-09-18 00:36:07 -0400

    alphanum only

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "aks-goof-registry"
+  name                = "aksGoofRegistry"
   resource_group_name = azurerm_resource_group.aks_goof.name
   location            = azurerm_resource_group.aks_goof.location
 }
